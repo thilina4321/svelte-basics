a31cfdefd834a1eba424eb4f3b35f21556d0e5c4
thilina_dilshan <thilina.dilshan@arimaclanka.com>
2021-10-22 16:49:16 +0530

    svelte basics

---
+++ src/components/feedback/feedbackList.svelte
@@ -0,0 +1,13 @@
+<script>
+    import Feedback from './feedback.svelte'
+    export let feedbacks = []
+    export let updateFeedback
+    export let deleteFeedback
+
+</script>
+
+<main>
+    {#each feedbacks as feedback (feedback.id)}
+        <Feedback {feedback} {updateFeedback} {deleteFeedback} />
+    {/each}
+</main>
